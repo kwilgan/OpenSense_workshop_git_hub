2ad6d0f9717544bd8cebe48197b2bbc84654c3cf
kwilgan <karina.wilgan@gmail.com>
2022-06-30 14:50:03 +0200

    functions.py

--- functions.py
+++ functions.py
@@ -20,6 +20,10 @@ def scatter_data(data,variable_1=None,variable_2=None):
         return
     data.plot.scatter(x=variable_1,y=variable_2)
     
-    
+  def scatter_data(data,variable_1=None,variable_2=None):
+    if (variable_1 is None) or (variable_2 is None):
+        print('please provide two variable names!')
+        return
+    data.plot.scatter(x=variable_1+1,y=variable_2)  
 
     
